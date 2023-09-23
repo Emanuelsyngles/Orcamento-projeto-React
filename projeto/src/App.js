4a83d0f2c5ca13278c0645907e1ef5e53358d8a6
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-09-23 16:59:26 -0300

    Adicionando os links

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,13 +1,14 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Newproject from './components/pages/Newproject';
 
 function App() {
   return (
     <Router>
       <ul>
-        <li>Home</li>
-        <li>Contado</li>
+        <Link to="/">Home</Link>
       </ul>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
