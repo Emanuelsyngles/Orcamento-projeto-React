b239200693dafedb278dcd188dad04307bc518c9
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-09-23 16:50:42 -0300

    Criando as rotas do projeto

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -2,9 +2,19 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contado</li>
+      </ul>
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/company"></Route>
+        <Route path="/contact"></Route>
+        <Route path="/newproject"></Route>
+      </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
